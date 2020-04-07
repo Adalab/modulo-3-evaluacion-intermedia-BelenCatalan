ea6b5f010d7af947a7cbf90e0bf5e1b17b402dc9
BelenCatalan <labdelante@gmail.com>
2020-04-07 09:12:12 +0200

    add object with all pokemon in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../stylesheets/App.css';
-import '../data/Plist.json';
+import pokemons from '../data/Plist.json';
+import Pokemon from './Pokemon';
+
+console.log(pokemons);
+const renderPokemons = () => {
+  return pokemons.map((pokemon) => {
+    return <Pokemon key={pokemon.id} id={pokemon.id} name={pokemon.name} types={pokemon.types} url={pokemon.url} evolution={pokemon.evolution} />;
+  });
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>Mi lista de pokemon</h1>
+        {renderPokemons()}
+        {/* <Pokemon id={pokemons.id} name={pokemons.name} types={pokemons.types} url={pokemons.url} evolution={pokemons.evolution} /> */}
       </header>
     </div>
   );
---
+++ src/components/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Pokemon(props) {
+  console.log(props);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi lista de pokemon</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Pokemon;
