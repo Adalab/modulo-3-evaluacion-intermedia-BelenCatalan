b3df8341d0626a3bc6ffcf165d858228cf6f52a0
BelenCatalan <labdelante@gmail.com>
2020-04-07 08:31:47 +0200

    create docs

---
+++ docs/precache-manifest.526c2cfe7e316dc70e01e2e89303f5ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0409293aeb8be57e04e194647bedb472",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ec603614b83908da595",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cb67aee5e91bfcf6846",
+    "url": "./static/js/2.6d402e5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6d402e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec603614b83908da595",
+    "url": "./static/js/main.0b580bbe.chunk.js"
+  },
+  {
+    "revision": "fa5b70473673cae622f8",
+    "url": "./static/js/runtime-main.f475111d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
